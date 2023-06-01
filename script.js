972ed5595d887526812ea0d4c1535e9b5516afd7
Elena Piaggio <elenapiaggio@gmail.com>
2023-06-01 23:32:26 +0200

    solve bug print correct range

--- script.js
+++ script.js
@@ -67,13 +67,13 @@ function printRange(num1, num2) {
   if (num1 === num2) {
     result.push(num1)
   } else if (num1 < num2) {
-    for (let i = num1; i < num2; i++) {
+    for (let i = num1; i <= num2; i++) {
       if (i % 2 === 0) {
         result.push(i);
       }
     }
   } else {
-    for (let i = num1 - 1; i > num2; i--) {
+    for (let i = num1; i >= num2; i--) {
       if (i % 2 !== 0) {
         result.push(i);
       }
